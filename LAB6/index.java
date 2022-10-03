f5887588e0c10a91cc699598068979ee894431bc
Saksham-Shahu <sakshamshahu.work@gmail.com>
2022-10-03 19:43:03 +0530

    AOK

---
+++ LAB6/index.java
@@ -0,0 +1,24 @@
+package LAB6;
+
+public class index {
+	public static void main (String[] args) {
+	Child obj1 = new Child();
+	Parent obj = new Parent();
+	obj.method1(5, 10);	
+	obj1.method1(7, 50);
+	obj1.method2(4,20);
+	}
+}
+
+class Parent{
+	public void method1(int a , int b) {
+		System.out.println(a+b);
+	}
+	private void method2(int a , int b) {
+		System.out.println(a*b);
+	}
+}
+
+class Child extends Parent{
+	
+}

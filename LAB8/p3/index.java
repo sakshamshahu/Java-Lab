4bcc5f861b923b7922fa661f4a0b099e48367271
Saksham-Shahu <sakshamshahu.work@gmail.com>
2022-11-06 14:50:40 +0530

    AOK

--- LAB8/p3/index.java
+++ LAB8/p3/index.java
@@ -1,5 +1,26 @@
 package LAB8.p3;
+import java.util.Scanner;
 
 public class index {
-
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the direction of cars on the single track (index number) \n1) Same\n2)Opposite");
+		String x=sc.nextLine();
+		
+		try{
+			if (x.equals("1")){
+				 System.out.println("No collision");
+			}
+			else if(x.equals("2")){
+	   throw new Exception("Cars moving in the opposite direction and may collide");
+			}
+			else {
+				System.out.println("Write 1 or 2");
+			}
+		}
+		catch(Exception e){
+		System.out.println(e);
+		}
+	}
 }
+
